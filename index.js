157a04cfb459679a6946971a1adacae1835bbb87
junifar <junifar@gmail.com>
2020-07-11 23:13:22 +0700

    2 files modified, 1 added, 1 deleted

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+
+import Header from './components/common/Header'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <Header/>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.querySelector('#root'));
